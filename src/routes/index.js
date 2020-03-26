961d5660bb754721ba6c2573f52b6b15d45395e5
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-26 00:21:33 -0300

    Added reference to incidents routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use('/ngos', require('./ngos'));
+router.use('/incidents', require('./incidents'));
 
 router.get('/', (request, response) => {
   response.json({
