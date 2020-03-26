83f6a5229ad61eb173f4e7f984ea88c5f4d5d8d3
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-26 00:47:14 -0300

    Adds implementation of DELETE /incidents/:id

--- src/controllers/incidents_controller.js
+++ src/controllers/incidents_controller.js
@@ -19,5 +19,25 @@ module.exports = {
     const incidents = await connection.table('incidents').select('*');
 
     return response.json(incidents);
-  }
+  },
+
+  async delete(request, response){
+    const { id } = request.params;
+    const ngo_id = request.headers.authorization;
+
+    const incident_ngo_id = await connection('incidents')
+      .select('ngo_id')
+      .where('id', id)
+      .first();
+
+    if (incident_ngo_id ==! ngo_id) {
+      return response.status(401).json();
+    }
+
+    await connection('incidents')
+      .where('id', id)
+      .delete();
+
+    return response.status(204).send();
+  },
 }
