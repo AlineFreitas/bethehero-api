aed2280baf1fcf96e217e366acb64c6ee79aa302
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-26 00:27:01 -0300

    Adds route to GET /incidents

--- src/routes/incidents.js
+++ src/routes/incidents.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const incidentsController = require('../controllers/incidents_controller')
 
 router.post('/', incidentsController.create);
+router.get('/', incidentsController.index);
 
 module.exports= router;
