426166f7027b0baf89f1b9e9f4763ef6766df080
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-25 22:33:22 -0300

    fixed whastapp reference to match migration value: whatsapp_number

--- src/routes/ngos.js
+++ src/routes/ngos.js
@@ -3,14 +3,14 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
 router.post('/', async(request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
+  const { name, email, whatsapp_number, city, uf } = request.body;
   const id = crypto.randomBytes(4).toString('HEX');
 
   await connection.table('ngos').insert({
     id,
     name,
     email,
-    whatsapp,
+    whatsapp_number,
     city,
     uf
   });
