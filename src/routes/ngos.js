351248166b834cf6815743acf8b30fd1d1e5cfb7
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-26 00:01:15 -0300

    Extracted business logic into controller and added reference to it

--- src/routes/ngos.js
+++ src/routes/ngos.js
@@ -1,35 +1,10 @@
 const router = require('express').Router();
-const crypto = require('crypto');
-const connection = require('../database/connection');
+const ngoController = require('../controllers/ngo_controller');
 
-router.post('/', async(request, response) => {
-  const { name, email, whatsapp_number, city, uf } = request.body;
-  const id = crypto.randomBytes(4).toString('HEX');
+router.post('/', ngoController.create);
 
-  await connection.table('ngos').insert({
-    id,
-    name,
-    email,
-    whatsapp_number,
-    city,
-    uf
-  });
+router.get('/', ngoController.index);
 
-  return response.json(id);
-});
-
-router.get('/', async(request, response) => {
-  const ngos = await connection.table('ngos').select('*');
-
-  return response.json(ngos);
-});
-
-router.get('/:id', async(request, response) => {
-  const id = request.params.id;
-
-  const ngo = await connection.table('ngos').select('*').where('id', id);
-
-  return response.json(ngo);
-});
+router.get('/:id', ngoController.show);
 
 module.exports = router
