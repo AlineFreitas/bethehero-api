7a4bb8fe12899b36337e98457647bdce0603188e
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-25 22:22:19 -0300

    Adds implementation for creating NGO on database with POST /ngos

--- src/routes/ngos.js
+++ src/routes/ngos.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const crypto = require('crypto');
+const connection = require('../database/connection');
 
-router.post('/', (request, response) => {
-  response.json();
+router.post('/', async(request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection.table('ngos').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json(id);
 });
 
 module.exports = router
