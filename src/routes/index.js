593c407175b736574e3d2face75fc8e08c315c72
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-25 21:52:58 -0300

    Removed users example route and it's reference in routes/index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-router.use('/users', require('./users'));
 router.use('/ngos', require('./ngos'));
 
 router.get('/', (request, response) => {
--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (request, response) => {
-  response.send("You've reached GET /users endpoint")
-});
-
-module.exports = router
