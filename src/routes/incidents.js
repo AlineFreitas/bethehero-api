3caea9b2744e2b7015239d95631c3372a3fa588a
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-26 00:30:09 -0300

    Adds route to DELETE /incidents/:id

--- src/routes/incidents.js
+++ src/routes/incidents.js
@@ -3,5 +3,6 @@ const incidentsController = require('../controllers/incidents_controller')
 
 router.post('/', incidentsController.create);
 router.get('/', incidentsController.index);
+router.delete('/:id', incidentsController.delete);
 
 module.exports= router;
