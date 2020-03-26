3dbe9bbc83ef432f69b9190a89e9c8480aabbb49
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-26 01:47:05 -0300

    Fixes spelling mistake on routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/ngos', require('./ngos'));
 router.use('/incidents', require('./incidents'));
+router.use('/sessions', require('./sessions'));
 
 router.get('/', (request, response) => {
   response.json({
