6dafb7f51bbf0c34dc6c13df34e769aef2e97dcf
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-29 18:01:03 -0300

    Added celebrate validation for GET /incidents requiring page query param to be numeric

--- src/routes/incidents.js
+++ src/routes/incidents.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const incidentsController = require('../controllers/incidents_controller')
 
 router.post('/', incidentsController.create);
-router.get('/', incidentsController.index);
+router.get('/', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+  }),incidentsController.index);
 router.delete('/:id', incidentsController.delete);
 
 module.exports= router;
