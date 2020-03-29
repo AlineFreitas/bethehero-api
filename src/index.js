1ed913ba7a0f2aac033ca1365de0c354b7e40405
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-29 17:49:30 -0300

    Updated src/routes/index.js to use celebrate errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 app.use(express.json());
 app.use(cors());
 
 app.use(require('./routes'));
+app.use(errors());
 
 module.exports = app
