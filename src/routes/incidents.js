a0fed499a9700a1e7fc9b14e3027a3aecf1896af
Aline Freitas <aline.do.freitas@gmail.com>
2020-03-29 18:23:59 -0300

    Added celebrate validation for DELETE and POST actions on incidents routes

--- src/routes/incidents.js
+++ src/routes/incidents.js
@@ -3,12 +3,25 @@ const { celebrate, Segments, Joi } = require('celebrate');
 
 const incidentsController = require('../controllers/incidents_controller')
 
-router.post('/', incidentsController.create);
+router.post('/', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required().max(150),
+    description: Joi.string().required(),
+    value: Joi.number().required(),
+    ong_id: Joi.string().required(),
+  })
+}), incidentsController.create);
+
 router.get('/', celebrate({
     [Segments.QUERY]: Joi.object().keys({
     page: Joi.number(),
   })
   }),incidentsController.index);
-router.delete('/:id', incidentsController.delete);
+
+router.delete('/:id', celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+  }).unknown()
+}), incidentsController.delete);
 
 module.exports= router;
